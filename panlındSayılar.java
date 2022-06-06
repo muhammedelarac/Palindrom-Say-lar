f8216129ecd1fc63b9a041e655c96aa9de6d9563
muhammed <alaa150alaa@.com>
2022-06-06 15:07:55 +0300

    tam

--- panlındSayılar.java
+++ panlındSayılar.java
@@ -1,23 +1,31 @@
+import java.util.Scanner;
 /**
  * panlındSayılar
  */
 public class panlındSayılar {
     static boolean isPalindrom(int number) {
-        int temp = number, reserveNumber = 0, lastNumber;
+        int temp = number, reverseNumber = 0, lastNumber;
+
         while (temp != 0) {
             lastNumber = temp % 10;
-            reserveNumber = (reserveNumber * 10) + lastNumber;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
             temp /= 10;
-
         }
-        if (number==reserveNumber)
+        if (number == reverseNumber) {
+            System.out.println("Palindrom sayıdır.");
             return true;
-        else
+        } else {
+            System.out.println("Palindrom sayı değildir.");
             return false;
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(isPalindrom(123454321));
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Bir sayı girin:");
+        int n = inp.nextInt();
+        System.out.println(isPalindrom(n));
+
+
     }
 }
-
