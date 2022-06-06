417f8dd3a2caae57f1b44038388cb8b4b0faf5ff
muhammed <alaa150alaa@.com>
2022-06-06 14:59:10 +0300

    first commit

---
+++ panlındSayılar.java
@@ -0,0 +1,23 @@
+/**
+ * panlındSayılar
+ */
+public class panlındSayılar {
+    static boolean isPalindrom(int number) {
+        int temp = number, reserveNumber = 0, lastNumber;
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reserveNumber = (reserveNumber * 10) + lastNumber;
+            temp /= 10;
+
+        }
+        if (number==reserveNumber)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(123454321));
+    }
+}
+
